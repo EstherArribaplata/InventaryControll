799ec0f030f1d12a0f2b490546a57f4112476cfa
HP <HP@DESKTOP-3NHF55T>
2025-01-04 14:29:53 -0500

    Agregar archivos de proyecto.

---
+++ InventaryControll/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventaryControll.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
